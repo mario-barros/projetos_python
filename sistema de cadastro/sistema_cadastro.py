a1926111a2ce744a16c9a5a69a9a696ebed6590c
mario_barros <mario.barros1205@gmail.com>
2024-10-12 16:10:11 +0100

    #Excluir usuario, #encerrar programa, nova função criada(valor_dicionario)

--- sistema de cadastro/sistema_cadastro.py
+++ sistema de cadastro/sistema_cadastro.py
@@ -1,12 +1,12 @@
-from valor_dicionario import valor_procurado, alterar_valor
+from valor_dicionario import valor_procurado, alterar_valor, excluir_usuario
 usuarios = []
 
 #Menu
 print('[1] Adicionar novo usuário\n[2] Listar todos os usuários\n[3] Actualizar informações de um usuário\n[4] Excluir um usuário\n[5] Sair do programa')
 
-#Adicionar novo usuário
 while True:
     opcao = int(input('\nEscolha uma opção: '))
+    #Adicionar novo usuário
     if opcao == 1:
         dict_user = {'Nome': input('\nDigite o seu nome: ').upper(), 
                     'Idade': int(input('Digite a sua idade: ')), 
@@ -40,9 +40,20 @@ while True:
                 print('Idade atualizada: ', nova_idade)
             elif opcao_atualizar == 3:
                 novo_email = input('Digite o novo email: ')
-                alterar_valor(usuarios, 'Nome', user, 'email', novo_email)
+                alterar_valor(usuarios, 'Nome', user, 'Email', novo_email)
                 print('Novo email: ', novo_email)
         else:
             print('Usuário não encontrado.')
     #Excluir usuário
-    
+    if opcao == 4:
+        user = input('Informe o nome do usuário: ').upper()
+        nome_do_usuario = valor_procurado(usuarios, user)
+        if nome_do_usuario == user:
+            excluir_usuario(usuarios, 'Nome', user)
+            print('Usuário excluido com sucesso!')
+        else:
+            print('Usuário não encontrado.')
+    #Encerrar o programa
+    if opcao == 5:
+        print('\nPrograma Encerrado.')
+        break
--- sistema de cadastro/valor_dicionario.py
+++ sistema de cadastro/valor_dicionario.py
@@ -8,3 +8,8 @@ def alterar_valor(lista2, chave_busca, valor_busca, chave_alterar, valor_alterar
     for dicionario2 in lista2:
         if dicionario2.get(chave_busca) == valor_busca:
             dicionario2[chave_alterar] = valor_alterar
+
+def excluir_usuario(lista3, chave_busca2, valor_busca2):
+    for indice, dicionario3 in enumerate(lista3):
+        if dicionario3[chave_busca2] == valor_busca2:
+            del lista3[indice]
